4a8bf3865d9501d9662717c9e45972b48f1b2406
unknown <chooliangyi6@gmail.com>
2024-06-16 16:23:19 +0800

    fix wifi not showing up on ui

--- example/lib/features/create_wifi/cubit/cubit/wifi_cubit.dart
+++ example/lib/features/create_wifi/cubit/cubit/wifi_cubit.dart
@@ -2,7 +2,7 @@ import 'package:checkapp_plugin_example/features/create_wifi/models/wifi.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WifiCubit extends Cubit<List<Wifi>> {
-  WifiCubit({List<Wifi>? initialWifi}) : super([]);
+  WifiCubit({List<Wifi>? initialWifi}) : super(initialWifi ?? []);
 
   void updateWifi({required List<Wifi> wifi}) {
     emit(wifi);
