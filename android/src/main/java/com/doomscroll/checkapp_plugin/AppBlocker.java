a6b7d28cd25f04d1cb3014da643b85cc98fa87a4
unknown <chooliangyi6@gmail.com>
2024-06-18 18:49:32 +0800

    refactor appblocker

--- android/src/main/java/com/doomscroll/checkapp_plugin/AppBlocker.java
+++ android/src/main/java/com/doomscroll/checkapp_plugin/AppBlocker.java
@@ -34,36 +34,19 @@ public class AppBlocker {
         boolean shouldCheckWifi = (boolean) toCheck.get("checkWifi");
         boolean shouldCheckDay = (boolean) toCheck.get("checkDay");
         boolean shouldCheckTiming = (boolean) toCheck.get("checkTiming");
-        List<String> currentDayTime = getCurrentDayTime();
-        String currentDay = currentDayTime.get(0);
-        String currentTiming24HFormat = currentDayTime.get(1);
+
 
         if (shouldCheckApp) {
             List<Map<String, Object>> apps = (List<Map<String, Object>>) toCheck.get("apps");
-            for (Map<String, Object> app : apps) {
-                String packageName = (String) app.get("packageName");
-                blockedAppInUsage = isAppInForeground(packageName);
-                break;
-
-
-            }
+            checkAppUsage(apps);
         }
 
         if (shouldCheckLocation) {
             List<Map<String, Object>> locations = (List<Map<String, Object>>) toCheck.get("locations");
             double currentLat = (double) toCheck.get("currentLat");
             double currentLng = (double) toCheck.get("currentLng");
+            checkBlockedLocation(locations, currentLat, currentLng);
 
-            for (Map<String, Object> location : locations) {
-                double longitude = (double) location.get("longitude");
-                double latitude = (double) location.get("latitude");
-                if (latitude == currentLat && longitude == currentLng) {
-                    atBlockedLocation = true;
-                    break;
-                }
-
-
-            }
 
         } else {
             atBlockedLocation = true;
@@ -71,48 +54,88 @@ public class AppBlocker {
         if (shouldCheckWifi) {
             List<String> wifis = (List<String>) toCheck.get("wifis");
             String currentWifi = (String) toCheck.get("currentWifi");
+            checkBlockedWifi(wifis, currentWifi);
 
-            for (String wifi : wifis) {
-                if (Objects.equals(wifi, currentWifi)) {
-                    usingBlockedWifi = true;
-                    break;
-                }
-            }
 
         } else {
             usingBlockedWifi = true;
         }
+        List<String> currentDayTime = getCurrentDayTime();
+
         if (shouldCheckDay) {
+            String currentDay = currentDayTime.get(0);
             List<String> days = (List<String>) toCheck.get("days");
-            for (String day : days) {
-                if (Objects.equals(day, currentDay)) {
-                    blockedDay = true;
-                    break;
-                }
-            }
-        }else {
-        blockedDay = true;
+            checkDay(days, currentDay);
+        } else {
+            blockedDay = true;
         }
 
         if (shouldCheckTiming) {
             List<Map<String, Object>> timings = (List<Map<String, Object>>) toCheck.get("timings");
 
+            String currentTiming24HFormat = currentDayTime.get(1);
 
-            for (Map<String, Object> timing : timings) {
-                String startTiming = (String) timing.get("startTiming");
-                String endTiming = (String) timing.get("endTiming");
-                if (isCurrentTimeWithinRange(startTiming, endTiming, currentTiming24HFormat)) {
-                    blockedTiming = true;
-                    break;
-                }
-
-
-            }
-        }else{
+            checkTiming(timings, currentTiming24HFormat);
+        } else {
             blockedTiming = true;
         }
-        return (usingBlockedWifi && atBlockedLocation && blockedAppInUsage && blockedDay && blockedTiming);
+        return shouldBlock();
 
     }
+    private static boolean shouldBlock() {
+        return usingBlockedWifi && atBlockedLocation && blockedAppInUsage && blockedDay && blockedTiming;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
+    private static void checkAppUsage(List<Map<String, Object>> apps) {
+        for (Map<String, Object> app : apps) {
+            String packageName = (String) app.get("packageName");
+            blockedAppInUsage = isAppInForeground(packageName);
+            if(blockedAppInUsage) {
+                break;
+            }
 
+        }
+    }
+
+    private static void checkBlockedLocation(List<Map<String, Object>> locations, double currentLat, double currentLng) {
+        for (Map<String, Object> location : locations) {
+            double longitude = (double) location.get("longitude");
+            double latitude = (double) location.get("latitude");
+            if (latitude == currentLat && longitude == currentLng) {
+                atBlockedLocation = true;
+                break;
+            }
+
+
+        }
+    }
+
+    private static void checkBlockedWifi(List<String> wifis, String currentWifi) {
+        for (String wifi : wifis) {
+            if (Objects.equals(wifi, currentWifi)) {
+                usingBlockedWifi = true;
+                break;
+            }
+        }
+    }
+
+    private static void checkDay(List<String> days, String currentDay) {
+        for (String day : days) {
+            if (Objects.equals(day, currentDay)) {
+                blockedDay = true;
+                break;
+            }
+        }
+    }
+
+    private static void checkTiming(List<Map<String, Object>> timings, String currentTiming24HFormat) {
+        for (Map<String, Object> timing : timings) {
+            String startTiming = (String) timing.get("startTiming");
+            String endTiming = (String) timing.get("endTiming");
+            if (isCurrentTimeWithinRange(startTiming, endTiming, currentTiming24HFormat)) {
+                blockedTiming = true;
+                break;
+            }
+        }
+    }
 }
